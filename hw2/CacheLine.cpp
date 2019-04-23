c90ce0a2ae7b42049fb72198704f11dd19736530
Yamin <yaminrf@gmail.com>
2019-04-23 16:43:28 +0300

    Added class for cache line.

---
+++ hw2/CacheLine.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Yamin on 4/23/2019.
+//
+
+#include "CacheLine.h"
---
+++ hw2/CacheLine.h
@@ -0,0 +1,15 @@
+//
+// Created by Yamin on 4/23/2019.
+//
+
+#ifndef COMPARCH_CACHELINE_H
+#define COMPARCH_CACHELINE_H
+
+
+class CacheLine {
+private:
+
+};
+
+
+#endif //COMPARCH_CACHELINE_H
