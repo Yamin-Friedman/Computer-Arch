199a2ff11b8ff886cee5734a45d4fedd80aa4fcf
Yamin <yaminrf@gmail.com>
2019-05-19 17:35:23 +0300

    Added correct file.

---
+++ hw3/dflow_calc.cpp
@@ -0,0 +1,61 @@
+/* 046267 Computer Architecture - Spring 2019 - HW #3 */
+/* Implementation (skeleton)  for the dataflow statistics calculator */
+
+#include "dflow_calc.h"
+#include <vector>
+
+using std::vector
+
+
+#define MAX_REGISTERS 32
+
+class DepNode {
+public:
+	DepNode():first_dep(NULL), second_dep(NULL), is_entry(false){};
+	bool check_is_entry(){ return is_entry;};
+	void set_entry_node(){is_entry = true;};
+
+
+	DepNode *first_dep;
+	DepNode *second_dep;
+	bool is_entry;
+};
+
+class DflowCtx {
+public:
+	DflowCtx() {
+		entry_node.set_entry_node();
+	}
+
+
+private:
+	unsigned int num_of_register_false_dependencies[MAX_REGISTERS] = {0};
+	vector<DepNode> dep_nodes;
+	DepNode entry_node;
+
+};
+
+ProgCtx analyzeProg(const unsigned int opsLatency[], const InstInfo progTrace[], unsigned int numOfInsts) {
+    return PROG_CTX_NULL;
+}
+
+void freeProgCtx(ProgCtx ctx) {
+}
+
+int getInstDepth(ProgCtx ctx, unsigned int theInst) {
+    return -1;
+}
+
+int getInstDeps(ProgCtx ctx, unsigned int theInst, int *src1DepInst, int *src2DepInst) {
+    return -1;
+}
+
+int getRegfalseDeps(ProgCtx ctx, unsigned int reg){
+	return -1;
+}
+
+int getProgDepth(ProgCtx ctx) {
+    return 0;
+}
+
+
